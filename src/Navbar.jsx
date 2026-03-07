e9db9bf6106b5bdb7508bb6e1d48e5455ac35b53
SA13605 <samyakpopulwad@gmail.com>
2026-03-07 14:58:50 +0530

    golo update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./navbar.css"
 import { Link } from 'react-router-dom';
+import logo from "../public/logo.webp"
+import userIcon from "../public/icons8-customer-50.png"
 
 function Navbar() {
   return (
     <>
 
       <nav className="navbar">
-        <div className="logo"><img src="../public/logo.webp" alt="Logo" className='logoImg'/></div>
+        <div className="logo"><img src={logo} alt="Logo" className='logoImg'/></div>
         <ul className="nav-links">
           <li><Link className='navLinks' to="/home">Home</Link></li>
           <li><Link className='navLinks' to="/services">Services</Link></li>
@@ -15,7 +17,7 @@ function Navbar() {
           <li><Link className='navLinks' to="/about">About</Link></li>
         </ul>
 
-        <img src="../public/icons8-customer-50.png" alt="User Icon" className='user-icon'/>
+        <img src={userIcon} alt="User Icon" className='user-icon'/>
       </nav>
 
 
